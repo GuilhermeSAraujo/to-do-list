32762efb3ddc06aefefa81a589fb2492bd473dee
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2022-03-20 02:21:13 -0300

    Update listaTasks.js

--- src/Components/ListaTasks/listaTasks.js
+++ src/Components/ListaTasks/listaTasks.js
@@ -3,11 +3,6 @@ import "./listaTasks.css";
 
 function ListaTasks({ listaTasks }) {
     let [existeTarefa, setExistTarefa] = useState(false);
-    if (listaTasks.length > 0) {
-        listaTasks.map((task) => {
-            return <h1>task.conteudo</h1>;
-        });
-    }
     console.log(listaTasks);
     return (
         <div className="lista">
