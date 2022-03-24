9c930fa053059a426e84a6adccaf4eb3fce35a77
Guilherme Souza Araújo <guilhermesouzae@hotmail.com>
2022-03-24 12:07:24 -0300

    Deploy correction

--- src/Components/IconeConcluir/iconeConcluir.js
+++ src/Components/IconeConcluir/iconeConcluir.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import IconeCheck from './iconeConcluir.svg'; // Path to your icons.svg
+import React from "react";
+import IconeCheck from "./iconeConcluir.svg"; // Path to your icons.svg
 
 const IconeConcluir = ({ concluirTask, taskId, tamanho }) => (
   <>
     <button onClick={() => concluirTask(taskId)}>
-      <img 
-      src={IconeCheck} 
-      alt="mySvgImage" 
-      width={tamanho} 
-      height={tamanho} />
+      <img src={IconeCheck} alt="mySvgImage" width={tamanho} height={tamanho} />
     </button>
   </>
 );
 
-export default IconeConcluir;
+export default IconeConcluir; //
